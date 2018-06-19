bbb214d9e577c59177808c9b7843904a7168d815
Andy Edwards <jedwards@fastmail.com>
2018-06-19 14:15:01 -0500

    fix: use Object.assign instead of lodash.assign

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
 /* eslint-env commonjs */
 
-import assign from 'lodash.assign'
-
 function createQuery({parse, stringify}) {
   function Query(...args) {
     if (!(this instanceof Query)) return new Query(...args)
     args.forEach(arg => {
-      if (typeof arg === 'string') assign(this, parse(arg))
-      else if (arg instanceof Object && !Array.isArray(arg) && !(arg instanceof Function)) assign(this, arg)
+      if (typeof arg === 'string') Object.assign(this, parse(arg) || {})
+      else if (arg instanceof Object && !Array.isArray(arg) && !(arg instanceof Function)) Object.assign(this, arg)
       else throw new Error('invalid argument: ' + arg)
     })
   }
